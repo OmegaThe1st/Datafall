cb8290e1c4b596b38b35aadfcf082881abf62af4
OmegaThe1st <kjpajayraj@gmail.com>
2026-08-19 23:44:08 -0400

    Play animations
    I made idle and walking animations and scripted the character to move and play the appropriate animations based on movement direction and velocity.

---
+++ scripts/player.gd
@@ -0,0 +1,39 @@
+extends CharacterBody2D
+
+
+const SPEED = 200.0
+var last_direction = Vector2.RIGHT
+
+@onready var anim_sprite: AnimatedSprite2D = $AnimatedSprite2D
+
+func _physics_process(delta: float) -> void:
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	process_movement()
+	move_and_slide()
+
+func process_movement() -> void:
+	var direction := Input.get_vector("left", "right", "up", "down")
+	
+	if direction != Vector2.ZERO:
+		velocity = direction * SPEED
+		last_direction = direction
+	else:
+		velocity = Vector2.ZERO
+	
+	process_anim(last_direction)
+
+func process_anim(direction) -> void:
+	if velocity != Vector2.ZERO:
+		play_anim("walk", direction)
+	else:
+		play_anim("idle", direction)
+
+func play_anim(prefix: String, dir: Vector2) -> void:
+	if dir.x != 0:
+		anim_sprite.flip_h = dir.x < 0
+		anim_sprite.play(prefix + "_right")
+	elif dir.y > 0:
+		anim_sprite.play(prefix + "_down")
+	elif dir.y < 0:
+		anim_sprite.play(prefix + "_up")
